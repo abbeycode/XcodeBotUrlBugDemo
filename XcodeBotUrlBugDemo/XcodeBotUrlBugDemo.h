71f6ee895b09798f792112e20940fe30f4139fa4
Dov Frankel <dov@abbey-code.com>
2014-12-30 13:03:37 -0500

    Initial Commit

---
+++ XcodeBotUrlBugDemo/XcodeBotUrlBugDemo.h
@@ -0,0 +1,19 @@
+//
+//  XcodeBotUrlBugDemo.h
+//  XcodeBotUrlBugDemo
+//
+//  Created by Dov Frankel on 12/30/14.
+//  Copyright (c) 2014 Abbey Code. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XcodeBotUrlBugDemo.
+FOUNDATION_EXPORT double XcodeBotUrlBugDemoVersionNumber;
+
+//! Project version string for XcodeBotUrlBugDemo.
+FOUNDATION_EXPORT const unsigned char XcodeBotUrlBugDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XcodeBotUrlBugDemo/PublicHeader.h>
+
+
